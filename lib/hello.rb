0755de6226ef6c2493836d0e84282024444df260
Nathan St.Amand <nathansaint810@gmail.com>
2020-02-18 16:02:29 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,10 +1,13 @@
 def hello_t(array)
+  if block_given?
   i = 0 
    while i < array.length 
     yield array[i]
     i += 1 
   end
-  return array
+  array
+else
+  put ""
 end
 
 # call your method here!
